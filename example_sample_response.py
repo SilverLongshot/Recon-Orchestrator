959cc707519bdf9131633f06356a0f78840b31f8
SilverLongshot <longshot_silver@yahoo.com>
2025-10-01 00:33:26 -0400

    Add files via upload

---
+++ example_sample_response.py
@@ -0,0 +1,5 @@
+HTTP/1.1 200 OK
+Content-Type: text/html; charset=UTF-8
+Content-Security-Policy: default-src *; script-src 'self' 'unsafe-inline'; object-src 'none'
+X-Frame-Options: SAMEORIGIN
+X-XSS-Protection: 1; mode=block
